447f1c7ce69110397b241b9fcf50e18d1a9649b0
Timna Schulze <Timna.lansel@gmail.com>
2023-01-22 09:14:24 -0800

    update constants with number variables

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,13 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
-saySomething("hi");
+const result = add(10,5);
+const outputText = "The sum is " + result + "."; 
 
-add(3, 5);
+const additionResult = add (10,5);
+window.alert(additionResult);
+
+const number1 = prompt("Enter a number:");
+const number2 = prompt("Enter another number:");
+
+window.alert(add(number1, number2));
